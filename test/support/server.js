6f3c31af45a3568be3366e101e3d99eda81df5ab
Sebastian Riedel <sri@cpan.org>
2021-03-28 13:44:06 +0200

    Try fd=0 on Windows

--- test/support/server.js
+++ test/support/server.js
@@ -6,4 +6,4 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World!');
 });
-server.listen({ fd: 3 });
+server.listen({ fd: 0 });
