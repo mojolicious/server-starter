1f0f1afba46ee1f5b2b27ff4550c2dd8fd5732ae
Daniel Mantovani <dmanto@gmail.com>
2021-04-04 10:23:39 -0300

    add slow server tests

--- test/start.js
+++ test/start.js
@@ -44,6 +44,37 @@ t.test('Do it again', async t => {
   t.equal(server.pid, null, 'stopped');
 });
 
+t.test('Slow server', async t => {
+  const server = await starter.newServer();
+  t.equal(server.pid, null, 'not started');
+  await server.launch('node', ['test/support/server.js', server.listenAddress(), 1000]);
+  t.equal(typeof server.pid, 'number', 'started');
+
+  const res = await fetch(server.url());
+  t.equal(res.ok, true, '2xx code');
+  t.equal(res.headers.get('Content-Type'), 'text/plain', 'right "Content-Type" header');
+  const buffer = await res.buffer();
+  t.equal(buffer.toString('utf8'), 'Hello World!', 'right content');
+
+  await server.close();
+  t.equal(server.pid, null, 'stopped');
+});
+
+t.test('Slow server, with wrong (too small) timeout', {skip: !process.env.MOJO_SERVER_STARTER_AVOID_FDPASS && process.platform !== 'win32'}, async t => {
+  const server = await starter.newServer();
+  t.equal(server.pid, null, 'not started');
+  await server.launch('node', ['test/support/server.js', server.listenAddress(), 1000], {connectTimeout: 500});
+  t.equal(typeof server.pid, 'number', 'started');
+
+  let err;
+  try { await fetch(server.url()); } catch (e) { err = e; }
+  t.ok(err, 'request failed');
+  t.equal(err.errno, 'ECONNREFUSED', 'right error');
+
+  await server.close();
+  t.equal(server.pid, null, 'stopped');
+});
+
 t.test('Use a specific port', async t => {
   const port = await getPort();
   const server = await starter.newServer(port);
