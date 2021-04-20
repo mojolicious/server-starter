5b37127aa49a8917c555e83d37b1aa711662dc60
Sebastian Riedel <sri@cpan.org>
2021-04-20 11:26:05 +0200

    Also lint support scripts

--- test/support/server_fd.js
+++ test/support/server_fd.js
@@ -3,7 +3,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
 });
-server.listen({ fd: 3 });
+server.listen({fd: 3});
